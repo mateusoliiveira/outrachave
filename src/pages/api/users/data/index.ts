2ceacf827c9f97807534c15b2d1fd1b05da8aaed
mateusoliiveira <teusolive@gmail.com>
2022-06-26 04:24:58 -0300

    Fixxxxx

--- src/pages/api/users/data/index.ts
+++ src/pages/api/users/data/index.ts
@@ -12,7 +12,11 @@ export default async function handler(
             const { data } = await ApiServer.get(`/users/data`, {
                 headers: { Authorization: 'Bearer ' + token }
             })
-            res.status(200).json(data)
+
+            const offers = await ApiServer.get(`/users/data/offers`, {
+                headers: { Authorization: 'Bearer ' + token }
+            })
+            res.status(200).json({ data, offers: offers.data })
         } catch (error: any) {
             res.status(error.response.status).json(error.response.data)
         }
