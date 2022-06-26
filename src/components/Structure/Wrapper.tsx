e50859c9738fb6cbd41a3213160790e458fc934d
mateusoliiveira <teusolive@gmail.com>
2022-06-26 18:43:11 -0300

    Feat: disclaimer

--- src/components/Structure/Wrapper.tsx
+++ src/components/Structure/Wrapper.tsx
@@ -9,6 +9,10 @@ const Wrapper = ({ children }: ReactChildren) => {
   return (
     <main className="bg-image min-h-100 min-h-screen globalFont">
       <NavbarComponent />
+      <div className="bg-gray-900 m-auto text-xs justify-center text-white">
+        site criado para fins de estudo sobre desenvolvimento web, todos os
+        anúncios são fictícios e as imagens de seus respectivos donos
+      </div>
       {children}
     </main>
   )
