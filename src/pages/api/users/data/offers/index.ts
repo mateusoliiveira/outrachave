54d1b046bdeb26615e5f135c21ecd3c2c9679571
mateusoliiveira <teusolive@gmail.com>
2022-06-26 02:49:42 -0300

    Fix account

--- src/pages/api/users/data/offers/index.ts
+++ src/pages/api/users/data/offers/index.ts
@@ -9,10 +9,10 @@ export default async function handler(
     const { token }: any = await getToken({ req })
     if (req.method == 'GET') {
         try {
-            const { data } = await ApiServer.get(`/users/data/offers`, {
+            return await ApiServer.get(`/users/data/offers`, {
                 headers: { Authorization: 'Bearer ' + token }
             })
-            return res.status(200).json(data)
+
         } catch (error: any) {
             res.status(error.response.status).json(error.response.data)
         }
