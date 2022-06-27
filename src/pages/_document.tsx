e6f479a602048f52690ce88daec7cd1d3355d8b5
mateusoliiveira <teusolive@gmail.com>
2022-06-26 23:41:55 -0300

    Fix: fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,7 +11,7 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;800&display=optional"
+            href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;800&display=swap"
             rel="stylesheet"
           />
           <link rel="shortcut icon" href="/img/favicon.png" />
