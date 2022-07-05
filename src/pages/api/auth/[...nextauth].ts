ff2a3e7d7c5783369d2f1518e5143093d5e84d2f
mateusoliiveira <teusolive@gmail.com>
2022-07-05 11:21:48 -0300

    Fix: login issues

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,28 +1,32 @@
 import NextAuth from "next-auth"
 import Credentials from "next-auth/providers/credentials"
-import { ApiClient, ApiServer } from "../../../_services"
-import { clientURL, serverURLApi } from "../../../lib/api"
-import axios from "axios"
-import { signIn } from "next-auth/react"
+import { ApiClient } from "../../../_services"
+import { clientURL } from "../../../lib/api"
 
 export default NextAuth({
   providers: [
     Credentials({
       type: 'credentials',
       async authorize(credentials: any, req): Promise<any> {
-        let {
-          data,
-          status
-        } = await
-            ApiClient
-              .post("/guests/login", credentials)
-        if (status === 200) {
-          return {
-            email: credentials?.username,
-            user: data.user.name,
-            token: data.token
-            //userRole: user.admin ? 'admin' : false,
+        try {
+          let {
+            data,
+            status,
+            ...resultRequest
+          } = await
+              ApiClient
+                .post("/guests/login", credentials)
+          console.log(resultRequest, data, status)
+          if (status === 200) {
+            return {
+              userRole: data.user.role === 1 ? 'admin' : false,
+              email: data.user.email,
+              user: data.user.name,
+              token: data.token,
+            }
           }
+        } catch (error: any) {
+          throw new Error('oops', { cause: error.response.data })
         }
         return null
       },
@@ -33,7 +37,7 @@ export default NextAuth({
   ],
   callbacks: {
     async jwt({ token, user }) {
-      //user?.userRole === 'admin' ? token.userRole = 'admin' : false
+      user?.userRole === 'admin' ? token.userRole = 'admin' : false
       user?.token != undefined ? token.token = user.token : false
       return token
     },
