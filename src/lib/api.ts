9bd347dc8308ec464209cac1ab52dd0032ea8db6
mateusoliiveira <teusolive@gmail.com>
2022-07-05 16:12:28 -0300

    fix: consts vars

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,11 +1,12 @@
-/*
+
 export const serverURLApi: string = 'https://outrachave.herokuapp.com/api/v1'
 export const callbackURLAfterLogin: string = 'https://outrachave.vercel.app/conta'
 export const clientURLApi: string = 'https://outrachave.vercel.app/api'
 export const clientURL: string = 'https://outrachave.vercel.app'
-*/
 
+/*
 export const serverURLApi: string = 'http://localhost:8000/api/v1'
 export const callbackURLAfterLogin: string = 'http://localhost:3000/conta'
 export const clientURLApi: string = 'http://localhost:3000/api'
 export const clientURL: string = 'http://localhost:3000'
+*/
